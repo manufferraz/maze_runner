9d2b1e394216370f73d23329493d46223dd70f94
Emanuella Ferraz <83237390+manufferraz@users.noreply.github.com>
2024-04-03 00:54:53 +0000

    maze 3 e 4 não funcionam

---
+++ maze_runner2.cpp
@@ -0,0 +1,165 @@
+#include <stdio.h>
+#include <stack>
+#include <cstdio>
+#include <iostream>
+#include <vector>
+#include <thread>
+#include <atomic>
+#include <mutex>
+
+char** maze = nullptr;
+int num_rows = 0;
+int num_cols = 0;
+
+struct pos_t {
+    int i;
+    int j;
+};
+
+std::stack<pos_t> valid_positions;
+std::atomic<bool> exit_found(false);
+std::mutex mtx; // Mutex global
+std::mutex print_mtx; // Mutex print
+
+
+using namespace std;
+
+pos_t load_maze(const char* file_name, char*** mazePtr, int& num_rows, int& num_cols) {
+    pos_t initial_pos = {-1, -1}; // Inicializa com valores inválidos
+    FILE *arquivo;
+
+    arquivo = fopen(file_name, "r");
+    if (arquivo == nullptr) {
+        std::cerr << "Não foi possível abrir o arquivo: " << file_name << std::endl;
+        return initial_pos;
+    }
+
+    if (fscanf(arquivo, "%d %d", &num_rows, &num_cols) != 2) {
+        std::cerr << "Erro ao ler as dimensões." << std::endl;
+        fclose(arquivo);
+        return initial_pos;
+    }
+
+    // Aloca a matriz do labirinto
+    *mazePtr = (char**)malloc(num_rows * sizeof(char*));
+    for (int i = 0; i < num_rows; ++i) {
+        (*mazePtr)[i] = (char*)malloc(num_cols * sizeof(char));
+    }
+
+    // Lê o labirinto do arquivo
+    for (int i = 0; i < num_rows; ++i) {
+        for (int j = 0; j < num_cols; ++j) {
+            char caractere = fgetc(arquivo);
+            if (caractere == '\n') {
+                caractere = fgetc(arquivo);
+            }
+            if (caractere == 'e') {
+                initial_pos.i = i;
+                initial_pos.j = j;
+            }
+            (*mazePtr)[i][j] = caractere;
+        }
+    }
+
+    fclose(arquivo); 
+    return initial_pos;
+}
+
+// Função que imprime o labirinto
+void print_maze(char** maze, int num_rows, int num_cols) {
+    //std::lock_guard<std::mutex> lock(print_mtx);
+    if (maze == nullptr) {
+        std::cerr << "O labirinto não foi carregado corretamente." << std::endl;
+        return;
+    }
+
+    for (int i = 0; i < num_rows; ++i) {
+        for (int j = 0; j < num_cols; ++j) {
+            std::cout << maze[i][j];
+        }
+        std::cout << std::endl;
+    }
+    std::cout << std::endl;
+}
+
+
+bool isValid(int i, int j) {
+    return i >= 0 && i < num_rows && j >= 0 && j < num_cols && (maze[i][j] == 'x' || maze[i][j] == 's');
+}
+
+
+bool walk(pos_t initial_pos) {
+    valid_positions.push(initial_pos); 
+
+    while (!valid_positions.empty()) {
+        if (exit_found) {
+            return false;
+        }
+        pos_t current_pos = valid_positions.top();
+        valid_positions.pop();
+
+        int i = current_pos.i;
+        int j = current_pos.j;
+
+        // Se a posição atual já foi visitada ou é inválida, pule para a próxima iteração
+        if (maze[i][j] != 'x' && maze[i][j] != 'e' && maze[i][j] != 's') continue;
+
+        // Verifica se a posição atual é a saída
+        if (maze[i][j] == 's') {
+            maze[i][j] = 'o'; // Marca a posição da saída
+            exit_found = true;
+            print_maze(maze, num_rows, num_cols);
+            return true;
+        } 
+
+        // Marca a posição atual como visitada
+        if (maze[i][j] == 'x') {
+            maze[i][j] = '.';
+        } else if (maze[i][j] == 'e') {
+            maze[i][j] = '.';
+        }
+        // Direções: direita, esquerda, baixo, cima
+        pos_t directions[4] = {{i, j + 1}, {i, j - 1}, {i + 1, j}, {i - 1, j}};
+
+        for (auto& dir : directions) {
+            if (isValid(dir.i, dir.j)) {
+                // Adiciona a posição válida e não visitada à pilha
+                valid_positions.push(dir);
+                thread ti(walk, dir);
+                ti.detach();
+            }
+        }
+
+        // Imprime o labirinto após cada passo
+        print_maze(maze, num_rows, num_cols);
+        //print_mtx.unlock();
+    }
+
+    return false; // Retorna falso se a saída não for encontrada
+}
+
+
+
+
+int main(    int argc, char* argv[]) {
+    //"../data/maze.txt"
+    pos_t initial_pos = load_maze( argv[1], &maze, num_rows, num_cols);
+
+    std::thread t1(walk, initial_pos);
+
+    t1.join();
+
+    if (exit_found) {
+        std::cout << "Saída encontrada!" << std::endl;
+    } else {
+        std::cout << "Saída não encontrada." << std::endl;
+    }
+
+    for (int i = 0; i < num_rows; ++i) {
+        free(maze[i]);
+    }
+
+    free(maze);
+
+    return 0;
+}
